f40715351b797ff4f5ab8a917791482792a8132c
wanjiku <annritawanjiku77@gmail.com>
2022-03-23 21:42:54 +0300

    put all contents in containers

---
+++ js/mombasa.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $(".hide-1").hover(function(){
+        $(".show-1").toggle()
+        $(".hide-1").toggle()
+    })
+
+    $(".hide-2").hover(function(){
+        $(".show-2").toggle()
+        $(".hide-2").toggle()
+    })
+
+    $(".hide-3").hover(function(){
+        $(".show-3").toggle()
+        $(".hide-3").toggle()
+    })
+
+    $(".hide-4").hover(function(){
+        $(".show-4").toggle()
+        $(".hide-4").toggle()
+    })
+
+   
+})
