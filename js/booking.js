6fe2a8e32f274b1412e8a7aba3e427dbf55b8243
John Powell <powell.john2011@gmail.com>
2022-03-23 09:51:33 +0300

    creating initial contents fo the booikng section

---
+++ js/booking.js
@@ -0,0 +1,9 @@
+function Booking(firstName, lastName,phone, email, date){
+    this.firstName = firstName;
+    this.lastName =lastName;
+    this.phone = phone;
+    this.email = email;
+    this.date = date
+}
+
+const myBookings = [];
