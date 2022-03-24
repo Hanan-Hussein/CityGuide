0a5688d6621b8f250183fd7c18d2273b430d1f14
wanjiku <annritawanjiku77@gmail.com>
2022-03-24 07:47:28 +0300

    add click JS function in attractions section and add five start rating in hacks section

--- js/mombasa.js
+++ js/mombasa.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
 
+    //hide&show attraction section
+    $(".hidecard1").click(function(){
+        $(".showcard1").fadeIn()
+        $(".hidecard1").fadeOut()
+    })
+    $(".showcard1").click(function(){
+        $(".hidecard1").fadeIn()
+        $(".showcard1").fadeOut()
+    })
+    $(".hidecard2").click(function(){
+        $(".showcard2").fadeIn()
+        $(".hidecard2").fadeOut()
+    })
+    $(".showcard2").click(function(){
+        $(".hidecard2").fadeIn()
+        $(".showcard2").fadeOut()
+    })
+    $(".hidecard3").click(function(){
+        $(".showcard3").fadeIn()
+        $(".hidecard3").fadeOut()
+    })
+    $(".showcard3").click(function(){
+        $(".hidecard3").fadeIn()
+        $(".showcard3").fadeOut()
+    })
+
+    // toggle mall section
     $(".hide-1").hover(function(){
         $(".show-1").toggle()
         $(".hide-1").toggle()
@@ -21,4 +48,5 @@ $(document).ready(function(){
     })
 
    
+
 })
