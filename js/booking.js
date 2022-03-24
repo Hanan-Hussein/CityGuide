a89d7eca40feec82e2b200f56b0614b41ed59eeb
John Powell <powell.john2011@gmail.com>
2022-03-24 15:55:08 +0300

    adding jquiery to collect user data

--- js/booking.js
+++ js/booking.js
@@ -1,14 +1,30 @@
-function Booking(firstName, lastName,phone, email,event, date){
+function Booking(firstName, lastName, phone , email, destination, startDate, endDate){
     this.firstName = firstName;
     this.lastName =lastName;
     this.phone = phone;
     this.email = email;
-    this.event = event;
-    this.date = date
+    this.destination = destination;
+    this.startDate = startDate;
+    this.endDate = endDate
 }
 
 const myBookings = [];
 
 $(document).ready(function(){
+    $(".tour-booking").on('click', (e)=>{
+        e.preventDefault();
+        let firstName = $("#firs-name").val();
+        let lastName = $("#last-name").val();
+        let phone = $("#tel").val();
+        let email = $("#email").val();
+        let destination = $("#destination").val();
+        let startDate = $("start-date").val();
+        let endDate = $("end-date").val();
+        const bookedTour = new Booking(firstName,lastName,phone,email,destination,startDate,endDate);
+        myBookings.push(bookedTour);
+    })
 
+    $(".tour-booking").on('click',()=>{
+        
+    })
 })
