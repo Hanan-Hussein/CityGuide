1883415cddf85399b46bbb41d03b4b99948eb557
Catherine <maritecatherine91@gmail.com>
2022-03-24 11:51:34 +0300

    add historical event images and some other changes|

---
+++ js/kisumu.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+   /* $(".btn#mode").click(function(event){
+        event.preventDefault();
+        $("body").css("background", "color:white;");
+        
+    });*/
+    $(".card img").click(function(){
+        $("#hide").hide();
+        $("#hide").show();
+    });
+});
